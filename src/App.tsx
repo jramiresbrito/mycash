089a03be008d933506c88bc1418f7cd7ccd28972
Joao Victor Ramires <joaoramires.engsoft@gmail.com>
2022-05-05 23:24:32 -0300

    Add Modal Structure, State and Styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
-import { GlobalStyle } from './styles/global';
+import Modal from 'react-modal';
+import { useState } from 'react';
 import { Header } from './components/Header';
 import { Dashboard } from './components/Dashboard';
 
+import { GlobalStyle } from './styles/global';
+import { NewTransactionModal } from './components/NewTransactionModal';
+
+Modal.setAppElement('#root');
+
 export function App() {
+  const [newTransactionModalIsOpen, setNewTransactionModalIsOpen] =
+    useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setNewTransactionModalIsOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setNewTransactionModalIsOpen(false);
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
+
+      <NewTransactionModal
+        isOpen={newTransactionModalIsOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
+
       <GlobalStyle />
     </>
   );
